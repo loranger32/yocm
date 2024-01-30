fb524e41e835bf644723b2202529f36edba91e7e
Laurent Guinotte <laurentguinotte@gmail.com>
2024-01-30 21:34:35 +0100

    GUI - Add feature to allow user to merge old followed publication with new enterprise records

--- db/models/user.rb
+++ db/models/user.rb
@@ -13,6 +13,28 @@ class User < Sequel::Model
     validates_format /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, :email, message: "is not a valid email"
   end
 
+  def can_merge_cbe_numbers?
+    !overlapping_enterprise_records.empty?
+  end
+
+  def merge_cbe_numbers_from_publications!
+    return false unless can_merge_cbe_numbers?
+
+    ets_to_add_count = overlapping_enterprise_records.count
+    pubs_to_delete = Publication.where(cbe_number: overlapping_enterprise_records.map(&:id)).all
+
+    DB.transaction do
+      (overlapping_enterprise_records - enterprises).each { add_enterprise(_1) }
+      pubs_to_delete.each { remove_publication(_1) }
+    end
+
+    "#{pubs_to_delete.count} publications merged into #{ets_to_add_count} enterprises"
+  end
+
+  def overlapping_enterprise_records
+    Enterprise.where(id: publications.map(&:cbe_number)).all
+  end
+
   def before_destroy
     remove_all_zip_codes
     remove_all_enterprises
