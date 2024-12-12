171850bc928a1d8e3017a55a8a8bf1b865fd6317
Laurent Guinotte <laurentguinotte@gmail.com>
2024-12-12 18:03:50 +0100

    ENGINE - Add User report - First steps

--- test/reporter_test.rb
+++ test/reporter_test.rb
@@ -5,39 +5,50 @@ require_relative "test_helpers"
 require_relative "../yocm/lib/engine" # for access to the ReportData struct
 require_relative "../yocm/lib/reporter_class"
 
+
+
 class ReportTest < Minitest::Test
   REPORTS_DIR = File.expand_path("test_data/reports", __dir__)
-  TEST_REPORT = File.join(REPORTS_DIR, "report_20220126.html")
+  FAKE_REPORT_DATE = "20220126"
+  TEST_REPORT = File.join(REPORTS_DIR, "report_#{FAKE_REPORT_DATE}.html")
 
-  # # Must be kept in sync with the struct defined in engine.rb
-  # ReportData = Struct.new(:start_time, :engine_version, :options, :target_date, :url,
-  #   :total_known, :total_unknown, :zip_code_errors, :total_new, :total_files,
+  # Must be kept in sync with the struct defined in engine.rb
+  # ReportData = Struct.new(:start_time, :engine_version, :options, :user, :no_user_option, :target_date,
+  #    :url, :total_known, :total_unknown, :zip_code_errors, :total_new, :total_files,
   #   :publications_saved, :ocr_scans_saved, :pngs_saved, :db_storage, :end_time, :elapsed_time)
 
-  def test_create_execution_report
+  # Allow setting if id field on User instances
+  User.strict_param_setting = false
+
+  DEFAULT_DATA = Yocm::Engine::ReportData.new(
+    start_time: Time.new(2022, 1, 27),
+    engine_version: "0.0.1",
+    options: {test: "test", blob: "blob"},
+    user: User.new(id: 1, email: "active_user@example.com", active: true),
+    no_user_option: false,
+    target_date: "2022/01/26",
+    url: "https://www.example.com",
+    total_known: 1111,
+    total_unknown: 2222,
+    zip_code_errors: 3333,
+    total_new: 4444,
+    total_files: 5555,
+    publications_saved: true,
+    ocr_scans_saved: "OCR Scans successfully saved",
+    pngs_saved: "PNG successfully saved",
+    db_storage: false,
+    end_time: Time.new(2022, 1, 27, 0, 1, 30),
+    elapsed_time: 90.0)
+
+  # hack to set an user id without touching DB
+  DEFAULT_DATA.user.id = 1
+  User.strict_param_setting = true
+
+  def test_create_execution_report_without_user_feature
     assert Dir.exist?(REPORTS_DIR)
     refute File.exist?(TEST_REPORT)
 
-    data = Yocm::Engine::ReportData.new
-
-    data.start_time = Time.new(2022, 1, 27)
-    data.engine_version = "0.0.1"
-    data.options = {test: "test", blob: "blob"}
-    data.target_date = "2022/01/26"
-    data.url = "https://www.example.com"
-    data.total_known = 1111
-    data.total_unknown = 2222
-    data.zip_code_errors = 3333
-    data.total_new = 4444
-    data.total_files = 5555
-    data.publications_saved = true
-    data.ocr_scans_saved = "OCR Scans successfully saved"
-    data.pngs_saved = "PNG successfully saved"
-    data.db_storage = false
-    data.end_time = Time.new(2022, 1, 27, 0, 1, 30)
-    data.elapsed_time = 90.0
-
-    Yocm::Reporter.create_report(data, REPORTS_DIR, "20220126")
+    Yocm::Reporter.create_report(DEFAULT_DATA, REPORTS_DIR, FAKE_REPORT_DATE)
 
     assert File.exist?(TEST_REPORT)
 
@@ -60,6 +71,52 @@ class ReportTest < Minitest::Test
     assert_includes report, "PNG successfully saved"
     assert_includes report, "90.0"
     assert_includes report, "2022-01-27 00:01:30 +0100"
+  end
+
+  def test_report_with_default_setting_and_active_user_present
+    Yocm::Reporter.create_report(DEFAULT_DATA, REPORTS_DIR, FAKE_REPORT_DATE)
+    report = File.read(TEST_REPORT)
+
+    assert_includes report, "No specific user selected, reports generated for the active user: active_user@example.com (id: 1)"
+  end
+
+  def test_report_with_no_user_option_explicitely_passed
+    data = DEFAULT_DATA.dup
+    data.no_user_option = true
+    data.user = nil
+
+    Yocm::Reporter.create_report(data, REPORTS_DIR, FAKE_REPORT_DATE)
+    report = File.read(TEST_REPORT)
+
+    assert_includes report, "\"No user\" option provided - no user report generated"
+  end
+
+  def test_report_with_default_setting_and_no_active_user_present
+    data = DEFAULT_DATA.dup
+    data.no_user_option = false
+    data.user = nil
+
+    Yocm::Reporter.create_report(data, REPORTS_DIR, FAKE_REPORT_DATE)
+    report = File.read(TEST_REPORT)
+
+    assert_includes report, "No specific user selected and no active user - no user report generated"
+  end
+
+  # No need to test for an non-existing user selected - program raise at the start if its the case.
+  def test_report_with_existing_user_selected
+    data = DEFAULT_DATA.dup
+    data.no_user_option = false
+
+    data.user = User.new(email: "selected@example.com", active: false)
+    # hack to set user id without touching DB
+    User.strict_param_setting = false
+    data.user.id = 2
+    User.strict_param_setting = true
+
+    Yocm::Reporter.create_report(data, REPORTS_DIR, FAKE_REPORT_DATE)
+    report = File.read(TEST_REPORT)
+
+    assert_includes report, "Specific user selected - report generated for user: selected@example.com (id: 2)"
   end
 
   def teardown
